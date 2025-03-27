1db6bff1292e80fc700fc6d37cbbd681220f4b5d
anujakumbhojkar <anujakumbhojkar@gmail.com>
2025-03-27 19:33:30 +0530

    Add files via upload

---
+++ dailybkp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SRC_DIR="/home/codespace/.local/bin/SRC_DIR"
+DST_DIR="/home/codespace/.local/bin"
+
+if [ ! -d "$SRC_DIR" ]; then
+    echo "Error: Source directory does not exist!"
+    exit 1
+fi
+
+if [ ! -d "$DST_DIR" ]; then
+    mkdir -p "$DST_DIR"
+    echo "Backup directory created"
+fi
+
+for file in "$SRC_DIR"/*; do
+    cp "$file" "$DST_DIR"
+echo 'Data backup done...!!!"
+done
+
+
